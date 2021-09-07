d7a1777b615c442ee5d3d3c80af7a29db15186c3
DJEHA MOHAMED <38737729+dr4mohamed@users.noreply.github.com>
2021-09-08 00:55:05 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/eth/geth --datadir "/home/eth/.ethereum" --http --http.addr 0.0.0.0  --http.api personal,eth,net,web3,admin,miner,txpool,debug
+./geth --datadir "./.ethereum" --http --http.addr 0.0.0.0  --http.api personal,eth,net,web3,admin,miner,txpool,debug
